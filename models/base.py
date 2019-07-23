852cffde18ee4053a6f6fc3437f6d053577d2b5c
AmyWilson <amydaniellewilson@gmail.com>
2019-07-23 16:28:46 +0100

    user model base done

--- models/base.py
+++ models/base.py
@@ -8,8 +8,17 @@ class BaseModel:
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def save(self):
+        self.updated_at = datetime.utcnow()
+
+        db.session.add(self)
+        db.session.commit()
+
+    def remove(self):
+        db.session.delete(self)
+        db.session.commit()
+
 class BaseSchema:
 
     created_at = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
     updated_at = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
-    
--- models/user.py
+++ models/user.py
@@ -1,7 +1,20 @@
-from app import db
+from app import db, ma
+from .base import BaseModel, BaseSchema
 
-class User(db.Model):
+class User(db.Model, BaseModel):
 
     __tablename__ = 'users'
 
-    
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    occupation = db.Column(db.String(50), nullable=False)
+    industry = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.Text, nullable=False, unique=True)
+
+class UserSchema(ma.Model, BaseSchema):
+
+    class Meta:
+        model = User
