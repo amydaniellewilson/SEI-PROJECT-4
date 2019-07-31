710c4a0346a0f9c99cf23e45943cdfec87bc34e3
AmyWilson <amydaniellewilson@gmail.com>
2019-07-31 14:44:43 +0100

    changed environment file

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE_URI', 'postgres://localhost:5432/networking')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/networking')
 secret = os.getenv('SECRET', 'my secret')
